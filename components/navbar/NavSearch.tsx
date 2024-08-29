936505ef88ae51361ad163952211473fa3287e2b
CR7sewey <miguelmotacarvalho@gmail.com>
2024-08-29 19:05:10 +0100

    NavSearch COmponent

--- components/navbar/NavSearch.tsx
+++ components/navbar/NavSearch.tsx
@@ -1,27 +1,44 @@
 "use client";
 import React, { useState } from "react";
 import { Input } from "../ui/input";
-import { redirect } from "next/navigation";
+import { redirect, useSearchParams } from "next/navigation";
 import { useRouter } from "next/navigation";
+import { useDebouncedCallback } from "use-debounce";
+import { useEffect } from "react";
 
 function NavSearch() {
-  const [inputValue, setInputValue] = useState("");
+  const searchParameters = useSearchParams();
+  console.log(searchParameters.get("search"));
+  const [search, setSearch] = useState(
+    searchParameters.get("search")?.toString() || ""
+  );
   const { replace } = useRouter();
-  const searchProduct = (value: string) => {
-    console.log("aqui a");
-    setInputValue(value);
-    if (value === "") {
-      replace(`/products`);
+  const searchProduct = useDebouncedCallback((value: string) => {
+    const urlParams = new URLSearchParams(searchParameters);
+    console.log("aqui a", urlParams.toString());
+    console.log(searchParameters);
+    setSearch(value);
+    //setInputValue(value);
+    if (value === null) {
+      urlParams.delete("search");
+      replace(`/products?${urlParams.toString()}`);
       return;
     }
-    replace(`/products?search=${inputValue}`);
-  };
+    urlParams.set("search", value);
+    replace(`/products?${urlParams.toString()}`); // toString turns the objet into layout=list&search=ava f.e.
+  }, 300);
+
+  useEffect(() => {
+    if (!searchParameters.get("search")) {
+      setSearch("");
+    }
+  }, [searchParameters.get("search")]);
   return (
     <Input
       type="search"
       className="max-w-xs dark:bg-muted "
       placeholder="search product ..."
-      value={inputValue}
+      value={search}
       onChange={(e) => searchProduct(e.target.value)}
     />
   );
